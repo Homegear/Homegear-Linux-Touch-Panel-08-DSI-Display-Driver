e1db205dba21a3ffffc6e8a1dadb6b6bc7213a7f
Adrian Roman <aroman_ro@yahoo.com>
2021-07-08 11:56:55 +0300

    Fixed a backlight bug that manifests when blanking the screen with newer kernels (including our custom one)

--- lcd_driver.c
+++ lcd_driver.c
@@ -995,7 +995,7 @@ static int hgltp08_unprepare(struct drm_panel *panel)
 #endif // NO_ENTER_SLEEP
 
     if (ctx->gpioBacklightD)
-        gpio_set_value_cansleep(ctx->backlightPin, 1);
+        gpio_set_value_cansleep(ctx->backlightPin, 0);
 
 #ifndef NO_ENTER_SLEEP
     msleep(CMD_DELAY_TIME);
@@ -1429,4 +1429,4 @@ module_mipi_dsi_driver(panel_hgltp08_dsi_driver);
 MODULE_LICENSE("GPL v2");
 MODULE_AUTHOR("Homegear GmbH <contact@homegear.email>");
 MODULE_DESCRIPTION("Homegear LTP08 Multitouch 8\" Display; black; WXGA 1280x800; Linux");
-MODULE_VERSION("1.0.27");
+MODULE_VERSION("1.0.28");
